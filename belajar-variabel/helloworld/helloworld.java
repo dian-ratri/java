b9ded7cd9c3304c643bddc9d84f7578f326ce3a3
Dian Ratri Hapsari <dianratrihapsari@gmail.com>
2019-12-05 12:51:58 +0700

    bikin class mobil dan bangun object mobil

--- belajar-variabel/helloworld/helloworld.java
+++ belajar-variabel/helloworld/helloworld.java
@@ -1,5 +1,22 @@
 public class helloworld{
     public static void main (String [] args){
         System.out.println("Hello World!");
+        // kita bangun object mobil
+        mobil bmw = new mobil();
+        bmw.merk = "BMW i3000";
+        bmw.maju();
+
+        mobil fordShelby = new mobil();
+        fordShelby.merk = "Ford Mustang Shelby";
+        fordShelby.maju();
+    }
+}
+
+class mobil{
+    //attribut
+    public String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" maju!! Brrmm..");
     }
 }
